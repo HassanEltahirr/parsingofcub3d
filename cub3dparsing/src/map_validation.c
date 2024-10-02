dc5941550b84d6e5eb9aa2be90c49cfe3d2d17d8
Rutsen1234 <132617865+Rutsen1234@users.noreply.github.com>
2024-10-02 22:01:27 +0400

    ss

--- cub3dparsing/src/map_validation.c
+++ cub3dparsing/src/map_validation.c
@@ -1,18 +1,23 @@
 #include "cub3d.h"
 #include <stdlib.h>
+
 int is_row_fully_walls(char *row)
 {
-	int i;
-	i = 0;
-	while(row[i])
-	{
-		if(row[i] != '1' || row[i] != ' ')
-		{
-			return 1;
-		}
-		i++;
-	}
-	return 0;
+    int i = 0;
+    while (row[i])
+    {
+        if (row[i] != '1' && row[i] != ' ')
+        {
+            return 1;  // Return 1 (error) if the row contains something other than '1' or ' '
+        }
+        i++;
+    }
+    return 0;
+}
+int is_valid_char(char c)
+{
+    // Valid characters adjacent to '0' are '0', '1', and player positions
+    return (c == '0' || c == '1' || c == 'N' || c == 'S' || c == 'E' || c == 'W');
 }
 int ismapenclosedbywalls(char **file_data)
 {
@@ -20,41 +25,54 @@ int ismapenclosedbywalls(char **file_data)
     int height = get_map_height(file_data);
 
     // Validate that the first and last rows are fully walled
-    if (!is_row_fully_walls(file_data[0]) || !is_row_fully_walls(file_data[height - 1]))
+    if (is_row_fully_walls(file_data[0]) || is_row_fully_walls(file_data[height - 1]))
     {
-        printf("Error1\nMap is not enclosed by walls\n");
+        printf("Error\nMap is not enclosed by walls (top or bottom row)\n");
         return -1;
     }
+
+october 14 linear algebra 19:35-21:00
+Wednesday, October 9, 2024 human behaviour 5 - 6:15
+October 11th 11:00am data science
+October 18th 11:am object orientated programming	quiz 2
 
     i = 1;
     while (i < height - 1) 
     {
-		int previous_width = ft_strlen(file_data[i - 1]);
         int width = ft_strlen(file_data[i]);
-		int next_width = ft_strlen(file_data[i + 1]);
-        j = 1;
-        while (j < width - 1)
+        j = 0;
+        while (j < width)
         {
-			if(j >= previous_width || j >= next_width)
-			{
-				printf("Error\nMap is not enclosed by walls\n");
-				return -1;
-			}
-            if (file_data[i][j] == '0') 
+            if (file_data[i][j] == '0' || file_data[i][j] == 'N' || file_data[i][j] == 'S' || file_data[i][j] == 'E' || file_data[i][j] == 'W')
             {
-                if (file_data[i - 1][j] != '1' && file_data[i - 1][j] != ' ')  
+                // Check for out-of-bounds
+                if (j == 0 || j == width - 1)
                 {
-                    printf("Error\nMap is not enclosed by walls (top)\n");
+                    printf("Error\nMap is not enclosed by walls (left or right edge)\n");
                     return -1;
                 }
-                if (file_data[i + 1][j] != '1' && file_data[i + 1][j] != ' ')  
+
+                // Check surrounding characters
+                if (!is_valid_char(file_data[i - 1][j]))
                 {
-					printf("%d, %d\n",i, j);
-					printf("ROW IS %s and its len is %d", file_data[i + 1], j);
-                    printf("Error\nMap is not enclosed by walls (bottom)\n");
+                    printf("Error\nMap is not enclosed by walls (top of position [%d][%d])\n", i, j);
                     return -1;
                 }
-                
+                if (!is_valid_char(file_data[i + 1][j]))
+                {
+                    printf("Error\nMap is not enclosed by walls (bottom of position [%d][%d])\n", i, j);
+                    return -1;
+                }
+                if (!is_valid_char(file_data[i][j - 1]))
+                {
+                    printf("Error\nMap is not enclosed by walls (left of position [%d][%d])\n", i, j);
+                    return -1;
+                }
+                if (!is_valid_char(file_data[i][j + 1]))
+                {
+                    printf("Error\nMap is not enclosed by walls (right of position [%d][%d])\n", i, j);
+                    return -1;
+                }
             }
             j++;
         }
@@ -65,46 +83,46 @@ int ismapenclosedbywalls(char **file_data)
 
 int get_map_height(char **file_data)
 {
-	int i;
-	i = 0;
-	while(file_data[i])
-	{
-		i++;
-	}
-	return i;
+    int i = 0;
+    while (file_data[i])
+    {
+        i++;
+    }
+    return i;
 }
+
 int parse_our_map(t_game_data *game_data ,char **file_data, int i)
 {
-	int j;
-	int player_found = 0;
-	i = 0;
+    int j;
+    int player_found = 0;
+    i = 0;
 
-	while (file_data[i])
-	{
-		j = 0;
-		while (file_data[i][j])
-		{
-			if(file_data[i][j] == 'S' || file_data[i][j] == 'N' || file_data[i][j] == 'W' || file_data[i][j] == 'E')
-			{
-				game_data->row = i;
-				game_data->coloumn = j;
-				game_data->directions = file_data[i][j];
-				file_data[i][j] = '0';
-				player_found = 1;
-			}
-			j++;
-		}
-		i++;
-	}
-	if (!player_found)
-{
-    printf("Error: No player found in the map.\n");
-    return -1;
-}
-	if(ismapenclosedbywalls(file_data) == -1)
-	{
-		return -1;
-	}
-	game_data->map = file_data;	
-	return 0;
+    while (file_data[i])
+    {
+        j = 0;
+        while (file_data[i][j])
+        {
+            if (file_data[i][j] == 'S' || file_data[i][j] == 'N' || file_data[i][j] == 'W' || file_data[i][j] == 'E')
+            {
+                game_data->row = i;
+                game_data->coloumn = j;
+                game_data->directions = file_data[i][j];
+                file_data[i][j] = '0';  // Replace player position with '0'
+                player_found = 1;
+            }
+            j++;
+        }
+        i++;
+    }
+    if (!player_found)
+    {
+        printf("Error: No player found in the map.\n");
+        return -1;
+    }
+    if (ismapenclosedbywalls(file_data) == -1)
+    {
+        return -1;
+    }
+    game_data->map = file_data;
+    return 0;
 }
